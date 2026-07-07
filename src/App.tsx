d3557b0d1d53263ac34e316be7613c5e3a5f01b5
lewislim <limyilewis@hotmail.com>
2026-07-07 18:26:16 +0800

    feat: add draco for mesh compression

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 import { GachaConfetti } from "./components/effects/GachaConfetti";
 import { CombiniScene } from "./components/scenes/CombiniScene";
 import { StreetScene } from "./components/scenes/StreetScene";
 import SceneControls from "./components/ui/SceneControls";
 import SceneToggle, { type Scene } from "./components/ui/SceneToggle";
+
+useGLTF.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/')
 
 export default function App() {
   const [scene, setScene] = useState<Scene>("combini");
